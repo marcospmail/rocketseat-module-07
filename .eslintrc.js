26f5c3be1720ec5167dd13e57b9c6362e5f904d2
Marcos Paulo <marcosp.mail@gmail.com>
2019-10-29 12:02:06 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn', {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/no-named-as-default': 0,
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 0,
+    "react/static-property-placement": ['error', 'property assignment', {
+      propTypes: 'static public field',
+    }]
+  },
+};
